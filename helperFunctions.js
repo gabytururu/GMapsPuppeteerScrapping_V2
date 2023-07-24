3d4ef3e6dcf1aa19656f5c504eea1ce7c20d7dd0
gabriela tururu <gabriela.aguayaire@gmail.com>
2023-07-24 16:28:03 -0600

    WIP_creating the place data verification function - not working due to CIRCULAR STRUCTURE BUG

--- helperFunctions.js
+++ helperFunctions.js
@@ -36,17 +36,135 @@ const titleCase = (placeName)=>{
     return titleCaseName 
 }
 
-const lowerCase = async(placeName) =>{
+const lowerCase = (placeName) =>{
     let lowerCase = placeName.toLowerCase()
     return lowerCase
 }
 
+const onlyPhoneMissing = (array) =>{
+    let phoneMissing;
+    if(array.find(arrElement => arrElement.includes('teléfono'))){
+        phoneMissing = true
+    }else{
+        phoneMissing = false
+    }
+    return phoneMissing
+}
+
+const onlyWebMissing = (array) =>  {
+    let webMissing;
+    if(array.find(arrElement => arrElement.includes('web'))){
+        webMissing = true
+    }else{
+        webMissing = false
+    }
+    return webMissing
+}
+
+const bothPhoneAndWebMissing = (array) =>{
+    let bothContactsMissing;
+    let phoneMissing = onlyPhoneMissing(array)
+    let webMissing = onlyWebMissing(array)
+
+    if (phoneMissing && webMissing){
+        bothContactsMissing = true
+    }else{
+        bothContactsMissing = false
+    }
+    return bothContactsMissing
+}
+
+
+const getPlaceDataInfo = (missingDataArray, placeData, dataSize, placeContacts) =>{
+
+    const {phone, web, address, city}  = infodelLugar
+    
+
+    if(placeData === null){
+        phone = 'No cuenta con teléfono'  
+        web = 'Web no disponible'              
+        address = 'No cuenta con dirección'
+        city = 'No cuenta con ciudad'
+    }else if(bothPhoneAndWebMissing(missingDataArray) === true && dataSize === 1){
+        phone = 'No cuenta con teléfono'  
+        web = 'Web no disponible'   
+        address = placeData[0].textContent
+        city = 'No cuenta con ciudad'  
+    }else if(bothPhoneAndWebMissing(missingDataArray) === true && dataSize === 2){
+        phone = 'No cuenta con teléfono'  
+        web = 'Web no disponible'              
+        address = placeData[0].textContent
+        city = placeData[1].textContent  
+    }else if(bothPhoneAndWebMissing(missingDataArray) === true && dataSize > 2){
+        phone = 'No cuenta con teléfono'  
+        web = 'Web no disponible'              
+        address = placeData[0].textContent
+        city = placeData[1].textContent
+        //extra = document.querySelectorAll('.Io6YTe.fontBodyMedium')[2].textContent  
+    }else if(bothPhoneAndWebMissing(missingDataArray) === false && onlyPhoneMissing(missingDataArray) === true && dataSize === 1){
+        phone = 'No cuenta con teléfono'  
+        web = placeContacts[0].href
+        address = placeData[0].textContent
+        city = 'No cuenta con ciudad'
+    }else if(bothPhoneAndWebMissing(missingDataArray) === false && onlyPhoneMissing(missingDataArray)=== true && dataSize ===2){
+        phone = 'No cuenta con teléfono'
+        web = placeContacts[0].href
+        address = 'No cuenta con dirección'
+        city = placeData[0].textContent
+    }else if(bothPhoneAndWebMissing(missingDataArray)===false && onlyPhoneMissing(missingDataArray)===true&&dataSize >2){
+        phone = 'No cuenta con teléfono'
+        web = placeContacts[0].href
+        address = placeData[0].textContent
+        city = placeData[2].textContent
+    }else if(bothPhoneAndWebMissing(missingDataArray)=== false && onlyWebMissing(missingDataArray) === true && dataSize === 1){
+        phone = placeData[0].textContent
+        web = 'Web no disponible' 
+        address = 'No cuenta con dirección'
+        city = 'No cuenta con ciudad'
+    }else if(bothPhAndWebMissing(missingDataArr)===false && onlyWebMissing(missingDataArr) === true && dataSize === 2){
+        phone = placeData[1].textContent
+        web = 'Web no disponible' 
+        address = placeData[0].textContent
+        city = 'No cuenta con ciudad'
+    }else if(bothPhAndWebMissing(missingDataArr)===false && onlyWebMissing(missingDataArr) === true && dataSize > 2){
+        phone = placeData[1].textContent
+        web = 'Web no disponible' 
+        address = placeData[0].textContent
+        city = placeData[2].textContent
+    }else if(onlyWebMissing(missingDataArr) === false && onlyPhoneMissing(missingDataArr) === false && dataSize === 2){
+        phone = placeData[1].textContent
+        web = placeContacts[0].href
+        address = 'No cuenta con dirección'
+        city = 'No cuenta con ciudad'
+    }else if(onlyWebMissing(missingDataArr) === false && onlyPhoneMissing(missingDataArr) === false && dataSize === 3){
+        phone = placeData[2].textContent
+        web = placeContacts[0].href
+        address = placeData[0].textContent
+        city = 'No cuenta con ciudad'
+    }else if(onlyWebMissing(missingDataArr) === false && onlyPhoneMissing(missingDataArr) === false && dataSize >= 4){
+        phone = placeData[2].textContent
+        web = placeContacts[0].href
+        address = placeData[0].textContent
+        city = placeData[3].textContent   
+    }else{
+        phone = 'caso excepcional revisar caso'
+        web = 'caso excepcional revisar caso'
+        address = 'caso excepcional revisar caso'
+        city = 'caso excepcional revisar caso' 
+    }
+    return infodelLugar
+}
+
+
 
 module.exports = {
-    autoScroll: autoScroll,
-    titleCase: titleCase,
-    lowerCase: lowerCase,
-    
+    autoScroll,
+    titleCase,
+    lowerCase,
+    onlyPhoneMissing,
+    onlyWebMissing,
+    bothPhoneAndWebMissing,
+    getPlaceDataInfo,
 }
 
   
--- scrapper.js
+++ scrapper.js
@@ -1,10 +1,10 @@
 const puppeteer = require('puppeteer');
-const {autoScroll, titleCase, lowerCase} = require('./helperFunctions');
+const {autoScroll, titleCase, lowerCase, onlyPhoneMissing,onlyWebMissing, bothPhoneAndWebMissing, getPlaceDataInfo} = require('./helperFunctions');
 
 
 (async()=>{
     let baseUrl = 'https://www.google.com.mx/maps/search/parques+ecoturisticos+en+baja+california+norte+OR+centros+ecoturisticos+en+baja+california+norte/@30.5249372,-115.8805746,7.5z/data=!4m2!2m1!6e1?entry=ttu'
-    let totalResults = 120
+    let totalResults = 3
     let searchTerm = 'parques ecoturisticos en baja california norte OR centros ecoturisticos en baja california norte'
     let typeOfPlace = 'Parque Ecoturístico'
     let corePlace = 'Baja California Norte'
@@ -15,6 +15,9 @@ const {autoScroll, titleCase, lowerCase} = require('./helperFunctions');
 
     const browser = await puppeteer.launch({headless:false})
     const page = await browser.newPage()
+    await page.exposeFunction('lowerCase', lowerCase)
+    await page.exposeFunction('titleCase', titleCase)
+    await page.exposeFunction('getPlaceDataInfo', getPlaceDataInfo)
     await page.setViewport({width:1300, height:900})
     await page.goto(`${baseUrl}`, {waitUntil: 'domcontentloaded'})
 
@@ -29,19 +32,13 @@ const {autoScroll, titleCase, lowerCase} = require('./helperFunctions');
             return placesLinks.slice(0,totalResults)
     },totalResults)
 
-    console.log ('The links list to found places is:', placesLinksList)
-    console.log ('The total number of found places is:', placesLinksList.length)
+    console.log ('The total number of found places is:', placesLinksList.length, 'and The links list to found places is:', placesLinksList)
 
     const placesData=[]
     let acct = 0
-
-    await page.exposeFunction('lowerCase', lowerCase)
-    await page.exposeFunction('titleCase', titleCase)
     for(let link of placesLinksList){
-        await page.goto(link)
-       
-        await page.waitForSelector('h1.DUwDvf.lfPIob')
-        //await page.exposeFunction('lowerCase', lowerCase)
+        await page.goto(link)       
+        await page.waitForSelector('h1.DUwDvf.lfPIob')      
     //     //compartir --> iframe//
     //     await page.waitForSelector('button.g88MCb.S9kvJb[data-value="Compartir"]')
     //     await page.click('button.g88MCb.S9kvJb[data-value="Compartir"]')
@@ -54,18 +51,27 @@ const {autoScroll, titleCase, lowerCase} = require('./helperFunctions');
     //     const iframeResized = mapWidth.replace('height="450"','height="420"')
     //     //END OF compartir --> iframe//
 
-    // --> ojo , al incluir iframe tmb debe incluirse como param en page eval->const placeSpecifics = await page.evaluate((typeOfPlace, corePlace,acct,targetWebsite, iframeResized,slug,titleCase, lowerCase)=>{
+            // --> ojo , al incluir iframe tmb debe incluirse como param en page eval->const placeSpecifics = await page.evaluate((typeOfPlace, corePlace,acct,targetWebsite, iframeResized,slug,titleCase, lowerCase)=>{
 
         const placeSpecifics = await page.evaluate(async(typeOfPlace, corePlace,acct,targetWebsite,slug)=>{
             const placeInfo = {}
-            const placeName = document.querySelector('h1.DUwDvf.lfPIob').textContent 
+            
+            const missingData = document.querySelectorAll('span.DkEaL')
+            const missingDataArray = []
+            for (let eachDataPoint of missingData){
+                missingDataArray.push(eachDataPoint.innerText)
+            }
+            const placeData = document.querySelectorAll('.Io6YTe.fontBodyMedium')
+            const dataSize = document.querySelectorAll('.Io6YTe.fontBodyMedium').length
+            const placeContacts = document.querySelectorAll('a.CsEnBe')
+         
 
-            // const titleCaseName = titleCase(placeName)
-            // const lowerCaseName = lowerCase(placeName)
+            
+            placeInfo.contacts = getPlaceDataInfo( missingDataArray, placeData, dataSize, placeContacts)
+            placeInfo.originalName = document.querySelector('h1.DUwDvf.lfPIob').textContent         
+            placeInfo.lowerCaseName = await lowerCase(placeInfo.originalName)
+            placeInfo.titleCaseName = await titleCase(placeInfo.originalName)  
 
-            placeInfo.name = placeName        
-            placeInfo.lowerCaseName = await lowerCase(placeName)
-            placeInfo.titleCaseName = await titleCase(placeName)        
             return placeInfo
         },typeOfPlace,corePlace,acct, targetWebsite,slug)
 
